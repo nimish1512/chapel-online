cb51d34f2fe7e4a0c99ab075644d53765f55e41c
Nimish Ronghe <nimish1512@gmail.com>
2017-03-21 16:11:00 +0530

    Update controller.py
    Added functionality for handling compilation error and notifying user

--- controller.py
+++ controller.py
@@ -1,13 +1,19 @@
 import sys
 import pwd
 import os
+from subprocess import Popen, PIPE
 filename = sys.argv[1]
 executable = filename+"_exec"
 command = '/home/nemo1521/chapel/chapel-1.14.0/bin/linux64/chpl -o '+executable+' '+filename
 uid = pwd.getpwnam("nemo1521").pw_uid
 change_owner = 'sudo bash sch.sh '+filename
 os.system(change_owner)
-os.system(command)
-run = './'+executable
-out = os.popen(run).readlines()
-print(out)
+error = Popen(['/home/nemo1521/chapel/chapel-1.14.0/bin/linux64/chpl','-o',executable,filename],stdout=PIPE,stderr=PIPE)
+op = error.communicate()
+if op[1]:
+	print(op[1])
+else:
+	run = './'+executable
+	execute = Popen([run], stdin=PIPE, stdout=PIPE)
+	out = repr(execute.stdout.readline())
+	print(str(out))		 
